71fd962e9a2b4e84f2900cce0fe7eab72a607e93
RonySan <ronysan940@gmail.com>
2021-06-02 23:53:41 -0300

    criando e estilizando painel.php, e comecando a criar painel de cadastro de alunos

--- painel.php
+++ painel.php
@@ -4,36 +4,68 @@ include('veri_login.php');
 ?>
 <html>
 	<head>
-		<title>Bem vindo Painel central </title>
-		<meta charset="UTF-8"/>
-		<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/>
-		<link rel="stylesheet" href="assets/css/grid.scss"/>
-	</head>
-	<body>
-		<div class="aria_menu">
-			<div class="menu">
-				<h2>ola Rony!!</h2>
-				<ul>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-				</ul>
+	<title>Bem vindo Painel central </title>
+	<meta charset="UTF-8"/>
+	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/>
+	<link rel="stylesheet" href="assets/css/grid.scss"/>
+	<link rel="stylesheet" href="assets/css/painel.css"/>
+</head>
+<header>
+	<div class="container">
+			<div class="logo">
+
 			</div>
-		</div>
-		
-		<section>
+			<div class="menu">
+				<nav>
+					
+					<ul>
+						<li><A HREF="">ola Rony!!</A></li>
+						<li><a href="">perfil</a></li>
+						<li>
+							<a href="logout.php">
+								<button>Sair</button>
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+	</div>
+</header>
+<body>
+	<section class="geral">
+		<div class="option0-content">
+		    <div class="conteiner column">
 
-		</section>
-		<div class="conteiner">
-			
-		</div>
+				<div class="option1">
+					<div class="grafico">
+						<h2>Grafico</h2>
+					</div>
+					<div></div>
+				</div>
 
-	<a href="logout.php">
-		<button>Sair</button>
-	</a>
+				<div class="option2">
+					<div>
+						<div class="alunos">
+							<h2>Alunos</h2>
+						</div>
+					</div>
+				</div>
 
+				<div class="option3">
+					<div class="usuarios">
+						<h2>Usuarios</h2>
+						<div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+	</section>
+	<div class="content">
+
+	</div>
 	<script type="text/javascript" scr="assets/js/jquery-3.6.0.min.js"></script>
 	<script type="text/javascript" scr="assets/js/bootstrap.bundle.js"></script>
-	</body>
+</body>
 </html>
